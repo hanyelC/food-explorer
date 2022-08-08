80485e68717ab238e3d9278481e396c0b40c75c4
hanyelC <hanyel.chamon@gmail.com>
2022-08-08 09:28:09 -0300

    feat(hooks): create hook to change theme

---
+++ src/hooks/theme.jsx
@@ -0,0 +1,30 @@
+import { createContext, useCallback, useContext, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { dark, light } from '../styles/themes'
+
+const ThemeContext = createContext({})
+
+export const useTheme = () => useContext(ThemeContext)
+
+export const CustomThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(dark)
+
+  const toggleTheme = useCallback(() => {
+    if (theme.name === 'dark') {
+      setTheme(light)
+    } else {
+      setTheme(dark)
+    }
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider
+      value={{ toggleTheme, theme }}
+    >
+      <ThemeProvider theme={theme}>
+        {children}
+      </ThemeProvider>
+
+    </ThemeContext.Provider>
+  )
+}
