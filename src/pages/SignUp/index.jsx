b1c6826446df6b831e19bdb3745550bbac8d04a9
hanyelC <hanyel.chamon@gmail.com>
2022-08-09 12:03:21 -0300

    feat(pages): create SignUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,71 @@
+import { useForm } from 'react-hook-form'
+
+import { Button } from '../../components/Button'
+import { Logo } from '../../components/Logo'
+import { Container, InputWrapper } from './styles'
+
+export function SignUp() {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = data => console.log(data)
+
+  return (
+    <Container>
+      <div>
+        <Logo size={50} />
+        <h1>food explorer</h1>
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Crie sua conta</h2>
+
+        <InputWrapper>
+          <label htmlFor="name">Seu nome</label>
+          <input
+            id='name'
+            type='text'
+            {...register('name')}
+            placeholder={'Exemplo: Maria da Silva'}
+          />
+        </InputWrapper>
+
+        <InputWrapper>
+          <label htmlFor="email">Email</label>
+          <input
+            id='email'
+            type='text'
+            {...register('email')}
+            placeholder={'Exemplo: exemplo@exemplo.com.br'}
+          />
+        </InputWrapper>
+
+        <InputWrapper>
+          <label htmlFor="password">Senha</label>
+          <input
+            id='password'
+            type='text'
+            {...register('password')}
+            placeholder={'No mínimo 6 caracteres'}
+          />
+        </InputWrapper>
+
+        <InputWrapper>
+          <label htmlFor="passwordConfirm">Insira a senha mais uma vez</label>
+          <input
+            id='passwordConfirm'
+            type='text'
+            {...register('passwordConfirm')}
+            placeholder={'No mínimo 6 caracteres'}
+          />
+        </InputWrapper>
+
+        <Button type='submit'>
+          Criar conta
+        </Button>
+
+        <span>Já tenho uma conta</span>
+      </form>
+
+    </Container>
+  )
+}
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+  align-items: center;
+  padding: 2rem;
+  color: #fff;
+  overflow: auto;
+ 
+  & div:first-child{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    font-size: 4.3rem;
+    font-weight: 700;
+    margin: 0 auto;
+    
+    h1 {
+      inline-size: min-content;
+    }
+  }
+
+  form {
+    width: 90%;
+    max-width: 48rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 3.2rem;
+    gap: 3.2rem;
+    border-radius: 1.6rem;
+
+    background-color: ${({ theme }) => theme.colors.BG.secondary};
+
+    & > h2 {
+      font-size: 3.2rem;
+      font-weight: 500;
+      font-family: ${({ theme }) => theme.FF.secondary}
+    }
+    
+    & > span {
+      font-size: 1.4rem;
+      font-weight: 500;
+      font-family: ${({ theme }) => theme.FF.secondary}
+    }
+  }
+`
+
+export const InputWrapper = styled.div`
+display: flex;
+flex-direction: column;
+gap: 8px;
+width: 100%;
+
+& > label {
+  font-size: 1.6rem;
+  font-weight: 400;
+  font-family: ${({ theme }) => theme.FF.primary} !important;
+  color: ${({ theme }) => theme.colors.text.secondary}
+}
+
+& > input {
+  font-size: 1.6rem;
+  font-weight: 400;
+  font-family: ${({ theme }) => theme.FF.primary};
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.colors.text.primary};
+  border-radius: 5px;
+  padding: 1.2rem 1.4rem;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.text.primary};
+}
+
+& > input::placeholder {
+  color: ${({ theme }) => theme.colors.text.secondary};
+}
+`
