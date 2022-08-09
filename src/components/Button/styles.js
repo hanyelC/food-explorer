2819d8330cb522c538d522d3193eefa3abdac7e4
hanyelC <hanyel.chamon@gmail.com>
2022-08-09 11:33:18 -0300

    feat(components): create Button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  background-color: ${({ theme }) => theme.colors.BG.btn.primary};
+  font-size: 1.4rem;
+  width: 100%;
+  font-weight: 500;
+  font-family: ${({ theme }) => theme.FF.secondary};
+  color: ${({ theme }) => theme.colors.text.primary};
+
+  border-radius: 5px;
+  padding: 1.2rem 0;
+`
