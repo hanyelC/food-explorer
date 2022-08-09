7d6ca704223374564a824fea3008abe197a373b1
hanyelC <hanyel.chamon@gmail.com>
2022-08-09 11:30:37 -0300

    feat: config the dark theme

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,17 +1,49 @@
 export const dark = {
   name: 'dark',
   colors: {
-    background: '#000A0F',
-    border: '#fad211',
-    text: '#837982'
-  }
+    BG: {
+      primary: '#000A0F',
+      secondary: '#00111a',
+      btn: {
+        primary: '#92000E',
+      },
+    },
+    border: {
+      primary: '#fad211',
+    },
+    text: {
+      primary: '#FFF',
+      secondary: '#C4C4CC'
+    }
+  },
+  FF: {
+    primary: '\'Roboto\', sans-serif',
+    secondary: '\'Poppins\', sans-serif',
+    tertiary: '\'DM Sans\', sans-serif'
+  },
 }
 
 export const light = {
   name: 'light',
   colors: {
-    background: '#FFFFF',
-    border: '#3e59e0',
-    text: '#dd3a27'
+    BG: {
+      primary: '#FFF',
+      secondary: '#FFF',
+      btn: {
+        primary: '#92000E',
+      },
+    },
+    border: {
+      primary: '#FFF',
+
+    },
+    text: {
+      primary: '#FFF'
+    },
+    FF: {
+      primary: '\'Roboto\', sans-serif',
+      secondary: '\'Poppins\', sans-serif',
+      tertiary: '\'DM Sans\', sans-serif'
+    },
   }
 }
