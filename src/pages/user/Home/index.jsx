857de02907a572e336dbe3157d1732d72a27872a
hanyelC <hanyel.chamon@gmail.com>
2022-08-10 17:17:29 -0300

    feat(pages): create Home

---
+++ src/pages/user/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../../../components/Header'
+
+import { Container } from './styles'
+
+export function Home() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  )
+}
