cc51599da6e5278f8a1884b2b8cfc8f93bb57015
hanyelC <hanyel.chamon@gmail.com>
2022-08-10 15:41:01 -0300

    feat(components): create Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles"
+
+export function Input({ icon: Icon, placeholder, ...rest }) {
+  return (
+    <Container>
+      {Icon && <Icon />}
+      <input
+        placeholder={placeholder}
+        {...rest}
+      />
+    </Container>
+  )
+}
---
+++ src/components/Input/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  gap: 1.4rem;
+
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.BG.tertiary};
+
+  border-radius: 10px;
+  
+  padding: 1.2rem 1.4rem;
+
+  & > input {
+    font-weight: 400;
+    font-size: 1.6rem;
+    line-height: 1.6rem;
+    
+    background-color: transparent;
+    border: none;
+    width: 100%;
+    
+    color: ${({ theme }) => theme.colors.text.primary};
+    font-family: ${({ theme }) => theme.FF.primary};
+  }
+
+  & > input::placeholder {
+    color: ${({ theme }) => theme.colors.text.tertiary};
+  }
+
+  & > svg {
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.colors.text.secondary};
+  } 
+`
