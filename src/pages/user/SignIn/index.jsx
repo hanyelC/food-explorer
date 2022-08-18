0dd2e0d932e0c3e6259804c8dedf4f6551232823
hanyelC <hanyel.chamon@gmail.com>
2022-08-18 17:01:24 -0300

    feat: implement sign in

--- src/pages/user/SignIn/index.jsx
+++ src/pages/user/SignIn/index.jsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form'
 import { isMobile } from 'react-device-detect'
 
+import { useAuth } from '../../../hooks/auth'
+
 import { Button } from '../../../components/Button'
 import { Logo } from '../../../components/Logo'
 import { ButtonText } from '../../../components/ButtonText'
@@ -10,7 +12,11 @@ import { Container, InputWrapper } from './styles'
 export function SignIn() {
   const { register, handleSubmit } = useForm()
 
-  const onSubmit = data => console.log(data)
+  const { signIn } = useAuth()
+
+  const handleSignIn = async data => {
+    await signIn(data)
+  }
 
   return (
     <Container isMobile={isMobile}>
@@ -19,7 +25,7 @@ export function SignIn() {
         <h1>food explorer</h1>
       </div>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(handleSignIn)}>
         <h2>Faça login</h2>
 
         <InputWrapper>
