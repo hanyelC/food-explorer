cee2378206ff2b2b1221ec7b9728b6c8823de43f
hanyelC <hanyel.chamon@gmail.com>
2022-11-03 13:35:14 -0300

    feat: get the product image

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -5,10 +5,13 @@ import { Counter } from '../Counter'
 import { Button } from '../Button'
 
 import { Container, Wrapper } from './styles'
+import { api } from "../../services/api";
+import { useEffect } from "react";
 
 export function ProductCard({ img, title, description, price }) {
   const [quantity, setQuantity] = useState(1)
   const [favorite, setFavorite] = useState(false)
+  const [imageUrl, setImageUrl] = useState(null);
 
   const handleFavorite = () => {
     setFavorite(prevState => !prevState)
@@ -22,29 +25,47 @@ export function ProductCard({ img, title, description, price }) {
     setQuantity(prevState => prevState + 1)
   }
 
+  const getImage = async () => {
+    try {
+      const response = await api.get(`/images/${img}`, {
+        responseType: "blob",
+      });
+      const url = URL.createObjectURL(response.data);
+
+      setImageUrl(url);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getImage();
+
+    return () => URL.revokeObjectURL(imageUrl)
+  }, []);
+
   return (
-    <Container>
-      <button onClick={handleFavorite}>
-        <FiHeart
-          className={favorite ? 'favorite' : ''}
-        />
-      </button>
+    imageUrl && (
+     <Container>
+        <button onClick={handleFavorite}>
+          <FiHeart className={favorite ? "favorite" : ""} />
+        </button>
 
-      <Wrapper>
-        <img src={img} />
-        <h3>{title} {'>'} </h3>
-        <p>{description}</p>
-        <span>R$ {price.toString().replace('.', ',')}</span>
-        <div>
-          <Counter
-            handleDecrease={handleDecrease}
-            handleIncrease={handleIncrease}
-            quantity={quantity}
-          />
-          <Button>incluir</Button>
-        </div>
-      </Wrapper>
-
-    </Container>
+        <Wrapper>
+          <img src={imageUrl} />
+          <h3>{title} {'>'} </h3>
+          <p>{description}</p>
+          <span>R$ {price.toFixed(2).replace('.', ',')}</span>
+          <div>
+            <Counter
+              handleDecrease={handleDecrease}
+              handleIncrease={handleIncrease}
+              quantity={quantity}
+            />
+            <Button>incluir</Button>
+          </div>
+        </Wrapper>
+      </Container>
+    )
   )
 }
