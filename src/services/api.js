842bbb76c8d8463461cfdb568a4d8c4bc5fd5702
hanyelC <hanyel.chamon@gmail.com>
2022-11-03 16:03:33 -0300

    chore: add api url as env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  // baseURL: 'https://food-explorer-api.herokuapp.com'
-})
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
