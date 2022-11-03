b81e8b8c38f9b2c32f7844df60532d4f2cabdbb5
hanyelC <hanyel.chamon@gmail.com>
2022-11-03 13:39:29 -0300

    style: add semicolons

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,7 +1,7 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css/bundle'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
 
-import { ProductCard } from '../ProductCard'
+import { ProductCard } from "../ProductCard";
 
 export function Slider({ products }) {
   return (
@@ -10,46 +10,43 @@ export function Slider({ products }) {
       slidesPerView={1}
       breakpoints={{
         400: {
-          slidesPerView: 1.3
+          slidesPerView: 1.3,
         },
         450: {
-          slidesPerView: 1.5
+          slidesPerView: 1.5,
         },
         500: {
-          slidesPerView: 1.7
+          slidesPerView: 1.7,
         },
         600: {
-          slidesPerView: 2
+          slidesPerView: 2,
         },
         750: {
-          slidesPerView: 2.5
+          slidesPerView: 2.5,
         },
         900: {
-          slidesPerView: 3
+          slidesPerView: 3,
         },
         1050: {
-          slidesPerView: 3.5
+          slidesPerView: 3.5,
         },
         1200: {
-          slidesPerView: 4
+          slidesPerView: 4,
         },
-
       }}
     >
-
-      {
-        products.map((prod) => (
-          <SwiperSlide
-            key={prod.id}>
+      {products.map((prod) => {
+        return (
+          <SwiperSlide key={prod.id}>
             <ProductCard
               description={prod.description}
-              img={prod.img}
-              title={prod.title}
+              img={prod.image.id}
+              title={prod.name}
               price={prod.price}
             />
           </SwiperSlide>
-        ))
-      }
+        );
+      })}
     </Swiper>
-  )
+  );
 }
