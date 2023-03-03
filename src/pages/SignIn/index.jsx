7e520946aebb6256b5429402cf5d15f038e9188a
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-03 11:19:59 -0300

    fix: imports

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,11 @@
 import { useForm } from 'react-hook-form'
 import { isMobile } from 'react-device-detect'
 
-import { useAuth } from '../../../hooks/auth'
+import { useAuth } from '../../hooks/auth'
 
-import { Button } from '../../../components/Button'
-import { Logo } from '../../../components/Logo'
-import { ButtonText } from '../../../components/ButtonText'
+import { Button } from '../../components/Button'
+import { Logo } from '../../components/Logo'
+import { ButtonText } from '../../components/ButtonText'
 
 import { Container, InputWrapper } from './styles'
 
