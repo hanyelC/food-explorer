667f45c9d0de1619518521f57e089f6588826e61
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-03 11:37:09 -0300

    feat: create 404 page

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 
-import { SignIn } from '../pages/user/SignIn'
-import { SignUp } from '../pages/user/SignUp'
+import { SignIn } from '../pages/SignIn'
+import { SignUp } from '../pages/SignUp'
+import { NotFound } from '../pages/NotFound'
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path='/' element={<SignIn />} />
       <Route path='/register' element={<SignUp />} />
+      <Route path='/*' element={<NotFound />}/>
     </Routes>
   )
 }
