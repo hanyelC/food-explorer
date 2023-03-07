3aeefc38df07a7672bb0b9726f88d79f87717ef5
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-07 10:03:04 -0300

    feat: add admin routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 
+import { useAuth } from '../hooks/auth'
+
 import { Home } from '../pages/Home'
+import { EditProduct } from '../pages/EditProduct'
+import { NewProduct } from '../pages/NewProduct'
 import { NotFound } from '../pages/NotFound'
 
 export function AppRoutes() {
+  const { user } = useAuth()
+
+  const isAdmin = user?.admin
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      {isAdmin && <Route path="/admin/new-product" element={<NewProduct />} />}
+      {isAdmin && <Route path="/admin/edit-product" element={<EditProduct />} />}
       <Route path="/*" element={<NotFound />} />
     </Routes>
   )
