4c347d19d899684ce2d36468e1b21619ac6f79a8
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-07 10:02:39 -0300

    feat: create EditProductPage

---
+++ src/pages/EditProduct/index.jsx
@@ -0,0 +1,80 @@
+import { FiUpload } from 'react-icons/fi'
+import { useNavigate } from 'react-router-dom'
+
+import { Header } from '../../components/Header'
+import { CaretLeft } from '../../components/Icons/CaretLeft'
+
+import {
+  BackButton,
+  Container,
+  Form,
+  InputBackground,
+  InputWrapper,
+  SubmitButton,
+  Wrapper,
+} from './styles'
+
+export function EditProduct() {
+  const navigate = useNavigate()
+
+  const handleNavigateBack = () => navigate(-1)
+
+  return (
+    <Container>
+      <Header />
+      <Wrapper>
+        <BackButton onClick={handleNavigateBack}>
+          <CaretLeft /> voltar
+        </BackButton>
+        <h2>Editar prato</h2>
+        <Form>
+          <div>
+            <InputWrapper>
+              <label htmlFor="image">Imagem do prato</label>
+              <InputBackground>
+                <FiUpload size={24} /> Selecione imagem
+                <input type="file" id="image" style={{ display: 'none' }} />
+              </InputBackground>
+            </InputWrapper>
+            <InputWrapper htmlFor="">
+              <label>Nome</label>
+              <InputBackground>
+                <input type="text" placeholder={'Ex.: Salada Ceasar'} />
+              </InputBackground>
+            </InputWrapper>
+            <InputWrapper htmlFor="" style={{ maxWidth: '36.4rem' }}>
+              <label>Categoria</label>
+              <InputBackground>
+                <input type="text" />
+              </InputBackground>
+            </InputWrapper>
+          </div>
+          <div>
+            <InputWrapper htmlFor="">
+              <label>Ingredientes</label>
+              <InputBackground>
+                <input type="text" />
+              </InputBackground>
+            </InputWrapper>
+            <InputWrapper htmlFor="" style={{ maxWidth: '25.1rem' }}>
+              <label>Preço</label>
+              <InputBackground>
+                <input type="number" placeholder={'R$ 00,00'} />
+              </InputBackground>
+            </InputWrapper>
+          </div>
+          <div>
+            <InputWrapper htmlFor="">
+              <label>Descrição</label>
+              <textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" />
+            </InputWrapper>
+          </div>
+
+          <SubmitButton disabled={true} type="submit">
+            Salvar alterações
+          </SubmitButton>
+        </Form>
+      </Wrapper>
+    </Container>
+  )
+}
---
+++ src/pages/EditProduct/styles.js
@@ -0,0 +1,159 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+
+  & > h2 {
+    font-family: ${({ theme }) => theme.FF.secondary};
+    color: ${({ theme }) => theme.colors.text.fifth};
+    font-weight: 500;
+    font-size: 3.2rem;
+    line-height: 1.4;
+
+    margin-bottom: 3.2rem;
+  }
+`
+
+export const BackButton = styled.button`
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.2rem;
+
+  margin-top: 4rem;
+  margin-bottom: 2.4rem;
+
+  background-color: transparent;
+
+  font-family: ${({ theme }) => theme.FF.secondary};
+  color: ${({ theme }) => theme.colors.text.fifth};
+  font-weight: 700;
+  font-size: 2.4rem;
+  line-height: 1.4;
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+
+  & > div {
+    display: flex;
+    gap: 3.2rem;
+
+    &:first-child {
+      & > div:first-child {
+        width: fit-content;
+        white-space: nowrap;
+      }
+    }
+  }
+`
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  width: 100%;
+
+  & > label {
+    font-family: ${({ theme }) => theme.FF.primary};
+    color: ${({ theme }) => theme.colors.text.secondary};
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 1;
+  }
+
+  & > textarea {
+    background-color: ${({ theme }) => theme.colors.BG.fifth};
+
+    padding: 1.4rem;
+
+    width: 100%;
+    height: 17.2rem;
+
+    border-radius: 8px;
+
+    color: #fff;
+    font-family: ${({ theme }) => theme.FF.primary};
+    font-weight: 400;
+    font-size: 1.6rem;
+
+    border-color: transparent;
+    appearance: none;
+    resize: none;
+
+    &:focus {
+      outline: #fff solid 1px;
+    }
+
+    &::placeholder {
+      color: ${({ theme }) => theme.colors.text.tertiary};
+    }
+  }
+`
+
+export const InputBackground = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  height: 4.8rem;
+  width: 100%;
+
+  background-color: ${({ theme }) => theme.colors.BG.fifth};
+
+  padding: 1.4rem 1.6rem;
+
+  border-radius: 8px;
+
+  &:focus-within {
+    outline: #fff solid 1px;
+  }
+
+  & > input {
+    font-weight: 400;
+    font-size: 1.6rem;
+    line-height: 1;
+
+    background-color: transparent;
+    border: none;
+    width: 100%;
+
+    color: ${({ theme }) => theme.colors.text.primary};
+    font-family: ${({ theme }) => theme.FF.primary};
+  }
+`
+
+export const SubmitButton = styled.button`
+  width: fit-content;
+
+  padding: 1.2rem 2.4rem;
+
+  border-radius: 5px;
+
+  background-color: ${({ theme }) => theme.colors.BG.btn.secondary};
+
+  color: #fff;
+  font-family: ${({ theme }) => theme.FF.secondary};
+  font-weight: 500;
+  font-size: 1.4rem;
+  line-height: 2.4rem;
+
+  align-self: flex-end;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.BG.btn.primary};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.BG.btn.disabled};
+  }
+`
